f40043838d17e1409338ed23ee21cb051110011e
Mehrdad Mehralian <dadwic0@gmail.com>
2024-04-29 21:38:47 +0300

    add Swipeable Drawer

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,4 +1,6 @@
-import * as React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import MuiAppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
@@ -6,21 +8,33 @@ import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import LockIcon from '@mui/icons-material/Lock';
 import MenuIcon from '@mui/icons-material/Menu';
+import Drawer from './Drawer';
 
 export default function AppBar() {
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Box sx={{ flexGrow: 1, direction: 'ltr' }}>
       <MuiAppBar position="static">
         <Toolbar>
           <LockIcon sx={{ mr: 1 }} />
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             rialir.com
           </Typography>
-          <IconButton>
+          <IconButton onClick={toggleDrawer(true)}>
             <MenuIcon />
           </IconButton>
         </Toolbar>
       </MuiAppBar>
+      <Drawer
+        open={open}
+        onOpen={toggleDrawer(true)}
+        onClose={toggleDrawer(false)}
+      />
     </Box>
   );
 }
---
+++ src/components/Drawer.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+
+export default function Drawer({ open, onClose, onOpen }) {
+  return (
+    <SwipeableDrawer
+      anchor="right"
+      open={open}
+      onClose={onClose}
+      onOpen={onOpen}
+      disableBackdropTransition
+    >
+      <Box role="presentation" onClick={onClose} onKeyDown={onClose}>
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+    </SwipeableDrawer>
+  );
+}
