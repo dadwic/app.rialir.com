738cf4add8ff91ecec17728f3e2cfecdcb2b288e
Mehrdad Mehralian <dadwic@yahoo.com>
2024-05-14 01:13:29 +0300

    remove Roboto font family

--- src/theme.js
+++ src/theme.js
@@ -1,12 +1,5 @@
 'use client';
-import { Roboto } from 'next/font/google';
 import { createTheme } from '@mui/material/styles';
-
-const roboto = Roboto({
-  weight: ['400', '700'],
-  subsets: ['latin'],
-  display: 'swap',
-});
 
 const theme = createTheme({
   direction: 'rtl',
@@ -14,7 +7,7 @@ const theme = createTheme({
     mode: 'dark',
   },
   typography: {
-    fontFamily: `Vazirmatn, ${roboto.style.fontFamily}`,
+    fontFamily: 'Vazirmatn',
   },
   components: {
     MuiAlert: {
