0e6decfd892b1c595fecbd2e9ffeba6a32bdcaf7
Mehrdad Mehralian <dadwic@yahoo.com>
2024-05-15 18:13:08 +0300

    dynamodb GetItemCommand params changes

--- src/app/api/rates/route.js
+++ src/app/api/rates/route.js
@@ -1,21 +1,20 @@
 import { DynamoDBClient, GetItemCommand } from '@aws-sdk/client-dynamodb';
 
-const client = new DynamoDBClient({
-  accessKeyId: process.env.ACCESS_KEY,
-  secretAccessKey: process.env.SECRET_ACCESS_KEY,
-  region: process.env.REGION,
-});
+const client = new DynamoDBClient({});
 
 export async function GET() {
   try {
-    const { Item } = await client.send(
+    const data = await client.send(
       new GetItemCommand({
         TableName: process.env.TABLE_NAME,
-        Key: { id: 1 },
+        Key: { id: { N: '1' } },
       })
     );
-    return Response.json(Item);
-  } catch (error) {
-    return Response.json({ error }, { status: 500 });
+    return Response.json(data.Item);
+  } catch (e) {
+    return Response.json(
+      { message: 'لطفاً بعداً دوباره امتحان کنید' },
+      { status: 500 }
+    );
   }
 }
