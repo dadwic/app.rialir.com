6cda5429ed0df577d819c7b2f28ed9d2e172d346
Mehrdad Mehralian <dadwic@yahoo.com>
2024-05-15 18:04:16 +0300

    DynamoDBClient set ACCESS_KEY and SECRET_ACCESS_KEY

--- src/app/api/rates/route.js
+++ src/app/api/rates/route.js
@@ -1,15 +1,17 @@
 import { DynamoDBClient, GetItemCommand } from '@aws-sdk/client-dynamodb';
 
-const client = new DynamoDBClient({});
+const client = new DynamoDBClient({
+  accessKeyId: process.env.ACCESS_KEY,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  region: process.env.REGION,
+});
 
 export async function GET() {
   try {
     const { Item } = await client.send(
       new GetItemCommand({
         TableName: process.env.TABLE_NAME,
-        Key: {
-          id: { S: req.query.id },
-        },
+        Key: { id: 1 },
       })
     );
     return Response.json(Item);
