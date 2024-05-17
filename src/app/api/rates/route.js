a37cd91c5e0895174ac6fdd7620a9958d04cecff
Mehrdad Mehralian <dadwic@yahoo.com>
2024-05-17 23:05:56 +0300

    add force-no-store

--- src/app/api/rates/route.js
+++ src/app/api/rates/route.js
@@ -3,6 +3,8 @@ import { GetCommand } from '@aws-sdk/lib-dynamodb';
 
 const client = new DynamoDBClient({});
 
+export const fetchCache = 'force-no-store';
+
 export async function GET() {
   try {
     const data = await client.send(
@@ -11,15 +13,12 @@ export async function GET() {
         Key: { id: 1 },
       })
     );
-    return Response.json(
-      { ...data.Item, time: new Date().toISOString() },
-      {
-        status: 200,
-        headers: {
-          'Cache-Control': 'no-cache, no-store, max-age=0, must-revalidate',
-        },
-      }
-    );
+    return Response.json(data.Item, {
+      status: 200,
+      headers: {
+        'Cache-Control': 'no-cache, no-store, max-age=0, must-revalidate',
+      },
+    });
   } catch (e) {
     return Response.error();
   }
