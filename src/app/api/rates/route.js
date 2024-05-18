26bda9e642e46952f129e342d6110bcd245e0dfc
Mehrdad Mehralian <dadwic@yahoo.com>
2024-05-18 11:09:53 +0300

    add revalidate 0 to api route

--- src/app/api/rates/route.js
+++ src/app/api/rates/route.js
@@ -11,15 +11,20 @@ export async function GET() {
         Key: { id: 1 },
       })
     );
-    return Response.json(data.Item, {
-      status: 200,
-      headers: {
-        'Cache-Control': 'max-age=60',
-        'CDN-Cache-Control': 'max-age=60',
-        'Vercel-CDN-Cache-Control': 'max-age=3600',
-      },
-    });
+    return Response.json(
+      { ...data.Item, updated_at: new Date().toISOString() },
+      {
+        status: 200,
+        headers: {
+          'Cache-Control': 'max-age=60',
+          'CDN-Cache-Control': 'max-age=60',
+          'Vercel-CDN-Cache-Control': 'max-age=3600',
+        },
+      }
+    );
   } catch (e) {
     return Response.error();
   }
 }
+
+export const revalidate = 0;
