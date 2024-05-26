ffd4811e3512e05fde948dbbaeb0cd94504770a2
Mehrdad Mehralian <dadwic@yahoo.com>
2024-05-26 18:26:40 +0300

    remove edge runtime api

--- src/app/api/rates/route.js
+++ src/app/api/rates/route.js
@@ -4,6 +4,9 @@ import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 
 const client = new DynamoDBClient({});
 
+export const fetchCache = 'force-no-store';
+export const revalidate = 0;
+
 export async function GET() {
   try {
     const data = await client.send(
@@ -17,6 +20,3 @@ export async function GET() {
     return NextResponse.error();
   }
 }
-
-export const runtime = 'edge';
-export const fetchCache = 'force-no-store';
