6f5f8e26cf022223c5fdaffdb2df20c9d8255989
Mehrdad Mehralian <dadwic@yahoo.com>
2024-06-02 15:02:01 +0300

    add lang menu in app bar

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,13 +1,24 @@
 import React, { useState } from 'react';
 import Box from '@mui/material/Box';
+import Menu from '@mui/material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import MuiAppBar from '@mui/material/AppBar';
+import MenuItem from '@mui/material/MenuItem';
 import MenuIcon from '@mui/icons-material/Menu';
 import IconButton from '@mui/material/IconButton';
+import TranslateIcon from '@mui/icons-material/Translate';
 import Drawer from './Drawer';
 
 export default function AppBar() {
   const [open, setOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const menuOpen = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   const toggleDrawer = (newOpen) => () => {
     setOpen(newOpen);
@@ -25,6 +36,27 @@ export default function AppBar() {
           >
             <img src="/logo.svg" height={48} />
           </Box>
+          <IconButton
+            id="lang-button"
+            aria-controls={menuOpen ? 'lang-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={menuOpen ? 'true' : undefined}
+            onClick={handleClick}
+          >
+            <TranslateIcon />
+          </IconButton>
+          <Menu
+            id="lang-menu"
+            anchorEl={anchorEl}
+            open={menuOpen}
+            onClose={handleClose}
+            MenuListProps={{
+              'aria-labelledby': 'lang-button',
+            }}
+          >
+            <MenuItem onClick={handleClose}>فارسی</MenuItem>
+            <MenuItem onClick={handleClose}>English</MenuItem>
+          </Menu>
           <IconButton onClick={toggleDrawer(true)}>
             <MenuIcon />
           </IconButton>
