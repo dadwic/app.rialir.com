8599421df04a94be794e2486eb42dc0f09248ede
Mehrdad Mehralian <dadwic@yahoo.com>
2024-06-02 17:04:26 +0300

    get locale using next cookies

--- src/i18n.js
+++ src/i18n.js
@@ -1,7 +1,9 @@
+import { cookies } from 'next/headers';
 import { getRequestConfig } from 'next-intl/server';
 
 export default getRequestConfig(async () => {
-  const locale = 'fa';
+  const cookieStore = cookies();
+  const locale = cookieStore.get('locale') || 'fa';
 
   return {
     locale,
