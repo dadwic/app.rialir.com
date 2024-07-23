6734379475798da7a0680bc161d140e711d23898
Mehrdad Mehralian <dadwic@yahoo.com>
2024-07-23 13:12:34 +0300

    remove privacy-policy from sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -6,11 +6,5 @@ export default function sitemap() {
       changeFrequency: 'hourly',
       priority: 1,
     },
-    {
-      url: 'https://app.rialir.com/privacy-policy',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
   ];
 }
